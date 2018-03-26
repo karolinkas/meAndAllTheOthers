50af31efee301061793d682a524feaf8e5c47bfd
karolin siebert <karolin@intellisense.io>
2018-03-27 01:14:35 +0200

    add basic router

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './index.css';
 
+//That's circular black button that shows the count of users/victims
 class Circle extends React.Component {
 
     state = {
         response: ''
     };
 
+    //That happens on page load I guess? 
     componentDidMount() {
         this.callApi()
             .then(res => this.setState({ response: res.count }))
             .catch(err => console.log(err));
         }
-
+        //To show the victim count we need to make a request to the backend
+        //where the current count is being calculated
         callApi = async () => {
         const response = await fetch('/api/others');
         const body = await response.json();
@@ -22,7 +26,8 @@ class Circle extends React.Component {
 
         return body;
     };
-
+  
+  //show victim count within a fancy circle 
   render () {
     return (
       <div className="circle">
@@ -33,7 +38,7 @@ class Circle extends React.Component {
     );
   }
 }
-
+//That's the line in the middle that has a bunch of buttons on it
 class MiddleBar extends React.Component {
   renderSquare () {
     return <Circle />;
@@ -42,9 +47,17 @@ class MiddleBar extends React.Component {
   render () {
     return (
       <div>
+        <h1>me & all the others</h1>
+        <h2>YOU ARE NOT ALONE</h2>
         <div className="middle-bar">
           {this.renderSquare()}
         </div>
+        <div className="column">
+        </div>
+        <div className="login">
+          <Link to="/login"><button className="login-button">LOG IN</button></Link>
+          <Link to="/joinus"><button className="login-button">JOIN US</button></Link>
+          </div>
         <div className="column">
         </div>
         <div className="column">
@@ -54,7 +67,31 @@ class MiddleBar extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <MiddleBar />,
+class Login extends React.Component {
+  render (){
+    return (
+      <div>This is where people login
+      </div>
+    );
+  }
+}
+class Join extends React.Component {
+  render (){
+    return (
+      <div>This is where people join us
+      </div>
+    );
+  }
+}
+
+ ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path="/" component={MiddleBar} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/joinus" component={Join} />
+      </div>
+  </Router>,
   document.getElementById('root')
 );
+
